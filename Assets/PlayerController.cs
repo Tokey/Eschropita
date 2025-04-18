78ff364c4e0173d174675a8b300334ec5f9b9b81
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2025-04-18 00:02:04 -0400

    3D world update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,36 +1,60 @@
-using System.Runtime.ConstrainedExecution;
 using UnityEngine;
-using UnityEngine.UIElements;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    private float moveSpeed = 1f;
+    [SerializeField]
+    private float gravity = -9.81f;
 
-    private float moveSpeed = 1;
     Animator animator;
-    Rigidbody2D rigidbody2d;
-
+    CharacterController cc;
     float scaleX;
-
-    // Start is called before the first frame update O references
+    Vector3 velocity;
 
     private void Start()
     {
-        animator = GetComponent<Animator>(); rigidbody2d = GetComponent<Rigidbody2D>();
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        cc = GetComponent<CharacterController>();
         scaleX = transform.localScale.x;
+        velocity = Vector3.zero;
     }
 
-void Update()
+    void Update()
     {
-        Vector2 velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * moveSpeed;
-        rigidbody2d.linearVelocity = velocity;
-        animator.SetFloat("Speed", velocity.sqrMagnitude);
+        // read input on XZ plane
+        Vector3 input = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0f,
+            Input.GetAxis("Vertical")
+        ).normalized;
 
-        // Flip Animation
+        // apply horizontal movement
+        Vector3 horizontalMove = input * moveSpeed;
 
-        Vector3 scale = transform.localScale; 
-        scale.x = scaleX*(velocity.x >= 0 ? 1 : -1);
-        transform.localScale = scale; 
+        // reset downward velocity when grounded
+        if (cc.isGrounded && velocity.y < 0f)
+            velocity.y = -2f;
+
+        // gravity
+        velocity.y += gravity * Time.deltaTime;
+
+        // move character
+        Vector3 move = horizontalMove + Vector3.up * velocity.y;
+        cc.Move(move * Time.deltaTime);
+
+        // update animator
+        animator.SetFloat("Speed", input.sqrMagnitude);
+
+        Debug.Log(input.sqrMagnitude);
+
+        // flip on X
+        if (input.x != 0f)
+        {
+            Vector3 s = transform.localScale;
+            s.x = scaleX * (input.x > 0 ? 1 : -1);
+            transform.localScale = s;
+        }
     }
 }
